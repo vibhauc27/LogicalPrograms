1815a3e03f44c1021924e7dc24dc11c9bebcf526
vibha <vibhauc@gmail.com>
2022-02-17 20:48:44 +0530

    first commit

---
+++ LogicalPrograms/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Class1
+    {
+        public void Fibonacci()
+        {
+            int num1 = 0, num2 = 1, num3;
+            
+            Console.WriteLine("Enter the number of Elements: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(num1 + " "); // prints value of 1st and 2nd 
+            Console.WriteLine(num2 + " ");
+
+            for (int i = 0; i < number; i++) 
+            {
+                num3 = num1 + num2;
+                Console.WriteLine(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+
+            }
+        }
+    }
+}
+    
+
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.Fibonacci();
+        }
+    }
+
+}
